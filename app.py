fbc8f714b9dccf6be7b0a8e41caa979c945c9bf2
sagsrinu <sagsrinu@gmail.com>
2026-04-06 07:09:15 +0530

    Implement basic HTTP server in app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello from EC2 🚀")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class AppHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from EC2 ")
+
+server = HTTPServer(("0.0.0.0", 8000), AppHandler)
+print("Server running...")
+server.serve_forever()
